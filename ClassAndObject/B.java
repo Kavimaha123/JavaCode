12d9032a1df4ff26022af97385700cf7a905a651
Kavimaha123 <kavimahadevan4@gmail.com>
2023-08-01 21:53:00 +0530

    ClassAndObject

---
+++ ClassAndObject/B.java
@@ -0,0 +1,17 @@
+package ClassAndObject;
+
+public class B{
+    protected float floatValue;
+
+    float FloatValue(){
+        return floatValue;
+    }
+
+    public float getFloatValue() {
+        return floatValue;
+    }
+
+    public void setFloatValue(float floatValue) {
+        this.floatValue = floatValue;
+    }
+}
---
+++ Inheritence/D.java
@@ -0,0 +1,14 @@
+package Inheritence;
+
+public class D {
+    int intD;
+
+    public int getIntD() {
+        return intD;
+    }
+
+    public void setIntD(int intD) {
+        this.intD = intD;
+    }
+    
+}
